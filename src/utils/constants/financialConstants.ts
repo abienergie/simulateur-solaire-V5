536208113dbcfdc3def99257db813d48e9347ac1
Abienergie <contact@abie.fr>
2025-10-31 22:45:28 +0100

    fix constants

--- src/utils/constants/financialConstants.ts
+++ src/utils/constants/financialConstants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_FINANCIAL_SETTINGS = {
-  baseKwhPrice: 0.26,
+  baseKwhPrice: 0.22,
   surplusSellPrices: {
     under9kw: 0.04,
     from9to36kw: 0.0617,
@@ -11,8 +11,8 @@ export const DEFAULT_FINANCIAL_SETTINGS = {
     from36to100kw: 0.0912
   },
   sellPriceDate: '2025-10-01',
-  defaultAutoconsumption: 75,
-  defaultEnergyRevaluation: 7,
+  defaultAutoconsumption: 60,
+  defaultEnergyRevaluation: 5,
   defaultSellIndexation: 2,
   defaultPanelDegradation: -0.2
 };
